50fbdf06cc14f3c3b10f790fb678ae9657ddd42a
GeoffMayhew-NOAA <geoff.mayhew@noaa.gov>
2026-02-13 19:44:33 +0000

    Added skip_promtp to gdrive_upload

--- R/gdrive_upload.R
+++ R/gdrive_upload.R
@@ -16,7 +16,7 @@
 #' \dontrun{
 #'   gdrive_upload(local_path, gdrive_dribble)
 #' }
-gdrive_upload <- function(local_path, gdrive_dribble) {
+gdrive_upload <- function(local_path, gdrive_dribble, skip_prompt = FALSE) {
 
   # Ensure googledrive token is active
   if(!gdrive_token()) return(invisible())
@@ -29,12 +29,17 @@ gdrive_upload <- function(local_path, gdrive_dribble) {
   if( l_path$ver_flag ) stop("Do not upload files with a version suffix!")
   # Parse the path to the google drive, looking for the files specified in local_path
   g_path <- parse_dribble(gdrive_dribble, l_path)
-
+  
   # Get the modify time of the local file
   local_mtime <- file.info(local_path)$mtime
   # convert this to the format Google wants
   new_mtime <- paste0(sub("(?<=[0-9])( )(?=[0-9])", "T", format(local_mtime, tz = "GMT"), perl = T), ".000Z")
-
+  
+  if(skip_prompt) {
+    upload_response <- "Y"
+    update_response <- "Y"
+  }
+  
   # Either upload or update the file
   if( nrow(g_path$gdrive_item) == 0 ) {
     # Upload File - If the file does not yet exist on the gdrive, upload the file to the gdrive
@@ -42,10 +47,12 @@ gdrive_upload <- function(local_path, gdrive_dribble) {
       crayon::cyan(local_path), " will be uploaded to ", crayon::yellow(gdrive_dribble$path),
       " as ", crayon::yellow("[ver1]"), ".\n"
     ))
-    upload_response <- rstudioapi::showPrompt(
-      title = "Notice!",
-      message = "Proceed with initial upload? (Y/N)"
-    )
+    if( !skip_prompt ) {
+      upload_response <- rstudioapi::showPrompt(
+        title = "Notice!",
+        message = "Proceed with initial upload? (Y/N)"
+      )
+    }
     if( is.null(upload_response) ) upload_response <- "N"
     upload_response <- toupper(upload_response)
     if( upload_response == "Y" ){
@@ -61,10 +68,10 @@ gdrive_upload <- function(local_path, gdrive_dribble) {
     }
   } else {
     # Update File - If the file already exists on the gdrive, and the local is ahead, update the file
-
+    
     # Compare the file sizes and modified dates of the local file and the head gdrive file
     compare_res <- compare_local_and_gdrive(l_path, g_path)
-
+    
     # Decide whether to update the existing file
     if( compare_res$identical ) {
       # *If the files are identical, don't bother updating!*
@@ -73,7 +80,7 @@ gdrive_upload <- function(local_path, gdrive_dribble) {
         crayon::yellow(paste0("[ver", g_path$current_ver, "]")), ". Skipping upload.\n"
       )))
     } else {
-
+      
       if( compare_res$local_status == "behind" ){
         # * If local is behind, prompt the user to check before proceeding*
         local_behind_response <- rstudioapi::showPrompt(
@@ -91,7 +98,7 @@ gdrive_upload <- function(local_path, gdrive_dribble) {
           stop("Aborting upload. Response was neither 'Y' or 'N'.")
         }
       }
-
+      
       # Local must be ahead of the gdrive.
       cat(paste0("Local is ", crayon::bold("ahead of")), paste0("the gdrive: [ver", g_path$current_ver, "]\n"))
       # Prepare to update
@@ -99,10 +106,12 @@ gdrive_upload <- function(local_path, gdrive_dribble) {
         crayon::bold(l_path$name), " in ", crayon::yellow(gdrive_dribble$path), " will be updated to ",
         crayon::yellow(paste0("[ver", g_path$current_ver + 1,  "]")), ".\n"
       ))
-      update_response <- rstudioapi::showPrompt(
-        title = "Notice!",
-        message = "Proceed with upload and update? (Y/N)"
-      )
+      if( !skip_prompt ) {
+        update_response <- rstudioapi::showPrompt(
+          title = "Notice!",
+          message = "Proceed with upload and update? (Y/N)"
+        )
+      }
       if( is.null(update_response) ) update_response <- "N"
       update_response <- toupper(update_response)
       if ( update_response == "N"){
